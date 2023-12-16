7b4a6026ecddd8e9036bdc8d8db7ba1da70b5335
Alexei Popov <alexeipopov.93@gmail.com>
2023-12-16 13:05:10 +0200

    added products context and styles initial shop view

--- src/index.jsx
+++ src/index.jsx
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
 import App from './App.jsx';
+
 import { UserProvider } from './contexts/user.context.jsx';
+import { ProductsProvider } from './contexts/products.context.jsx';
 
 import './index.scss';
 
@@ -11,7 +13,9 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <UserProvider>
-        <App />
+        <ProductsProvider>
+          <App />
+        </ProductsProvider>
       </UserProvider>
     </BrowserRouter>
   </React.StrictMode>
